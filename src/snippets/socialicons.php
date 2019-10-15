471bddbbe6efaf3d3c82b3e25da8bdbd691429eb
Luke Hatfield <luke@lukehatfield.com>
2019-10-15 11:51:31 -0400

    use padding to make mobile icons larger

--- src/snippets/socialicons.php
+++ src/snippets/socialicons.php
@@ -1,18 +1,16 @@
 
 <div class="footer-social-media">
   <div class="footer-social-media-title">Follow</div>
-  <ul class="footer-social-media-list">
-    <li class="footer-social-media-item"><a href="https://www.facebook.com/CentralSquareMA/">
-        <svg viewBox="0 0 44 44" class="footer-social-media-icon">
-          <use xlink:href="#facebook"></use>
-        </svg></a></li>
-    <li class="footer-social-media-item"><a href="https://www.instagram.com/centralsqbid/">
-        <svg viewBox="0 0 44 44" class="footer-social-media-icon">
-          <use xlink:href="#instagram"></use>
-        </svg></a></li>
-    <li class="footer-social-media-item"><a href="https://twitter.com/centralsqbid?lang=en">
-        <svg viewBox="0 0 44 44" class="footer-social-media-icon">
-          <use xlink:href="#twitter"></use>
-        </svg></a></li>
-  </ul>
+  <div class="footer-social-media-list">
+    <!-- no li b/c a tag needs to be wrapper so can use susy grid measurements of parent for padding to match negative margins--><a href="<?= option('visualdialogue.socialicons.facebook') ?>" target="_blank" class="footer-social-media-link">
+      <svg viewBox="0 0 44 44" class="footer-social-media-icon">
+        <use xlink:href="#facebook"></use>
+      </svg></a><a href="<?= option('visualdialogue.socialicons.instagram') ?>" target="_blank" class="footer-social-media-link">
+      <svg viewBox="0 0 44 44" class="footer-social-media-icon">
+        <use xlink:href="#instagram"></use>
+      </svg></a><a href="<?= option('visualdialogue.socialicons.twitter') ?>" target="_blank" class="footer-social-media-link">
+      <svg viewBox="0 0 44 44" class="footer-social-media-icon">
+        <use xlink:href="#twitter"></use>
+      </svg></a>
+  </div>
 </div>
